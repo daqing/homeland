b4b1cdf4652d18bbf1ff1683c27c53168cec3f76
David Zhang <daqing@mindgee.io>
2024-05-03 18:55:00 +0800

    Add node id to tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,8 @@
 class Tag < ApplicationRecord
   validates :name, :key, presence: true
 
+  belongs_to :node
+
   def self.sorted
     order("position ASC, created_at ASC")
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_03_103018) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_03_104940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -260,6 +260,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_03_103018) do
     t.text "intro"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "node_id"
   end
 
   create_table "team_users", id: :serial, force: :cascade do |t|
